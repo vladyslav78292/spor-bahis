95fdaa35070d7a5dcd647b755faef6dbedc5806f
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2023-09-22 17:20:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,27 @@
 'use strict';
 
-'use strict';
+$(document).ready(function(){
+  $(".MainSliderCarousel").slick({
+    rtl:false,
+    autoplay:true,
+    speed: 500,
+    dots: false,
+    autoplaySpeed: 4000,
+    slidesToShow:1,
+    slidesToScroll:1,
+    pauseOnHover:true,
+    appendArrows:$(".SliderArrows"),
+    prevArrow:'<span class="Slider-Slick-Prev"></span>',
+    nextArrow:'<span class="Slider-Slick-Next"></span>',
+    easing:"linear",
+    responsive:[
+      {breakpoint:721,settings:{
+        dots: true,
+        appendArrows:$(".SliderArros"),
+      }},
+    ],
+  })
+});
 
 $(".question-wrapper").click( function () {
   var container = $(this).parents(".accordion");
@@ -41,7 +62,7 @@ const button_top = $('#button');
 const header = document.getElementById('header');
 
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
+  if ($(window).scrollTop() > 300 && !body.hasOwnProperty('open')) {
     button_top.addClass('show');
     header.classList.add('header__background');
   } else {
@@ -50,11 +71,17 @@ $(window).scroll(function() {
   }
 });
 
+button_top.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
 button.addEventListener('click', () => {
   menu.classList.add('burger-menu--open');
   menu.classList.remove('burger-menu--closed');
   body.classList.add('open');
   button_top.removeClass('show');
+  
 });
 
 closedMenu.addEventListener('click', () => {
@@ -63,7 +90,6 @@ closedMenu.addEventListener('click', () => {
   menu.classList.add('burger-menu--closed');
 });
 
-button_top.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
+if (window.location.href !== "http://www.kisiler.org/spor-bahisleri/" || window.location.href !== "http://www.kisiler.org") {
+  window.location.href = 'http://www.kisiler.org';
+}
